02087435f7eb5c76a797c44bb1bc4e1f921cf3d9
yejiCho <goe152@naver.com>
2021-05-13 11:16:31 +0900

    Creat Hash PR 42576

---
+++ yeji/PR_42576.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+    public String solution(String[] participant, String[] completion) {
+        
+        Arrays.sort(participant);
+        Arrays.sort(completion);
+        
+        int i;
+        for(i=0; i<completion.length; i++){
+            if(!completion[i].equals(participant[i])){
+                return participant[i];
+            }
+            
+        }
+        
+        return participant[i];
+    }
+}
