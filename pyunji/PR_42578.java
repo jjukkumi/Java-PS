b5819684a6f3d632cdad87b14da9ae01db28221f
Pyunji <vusguswl@gmail.com>
2021-05-17 14:49:11 +0900

    Update Stream적용

--- pyunji/PR_42578.java
+++ pyunji/PR_42578.java
@@ -1,24 +1,12 @@
-import java.util.*;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 class Solution {
     public int solution(String[][] clothes) {
-        Map<String, ArrayList> cloMap = new HashMap<>();
-        for (int i = 0; i < clothes.length; i++) {
-            if (cloMap.containsKey(clothes[i][1])) {
-                ArrayList oldVal = cloMap.get(clothes[i][1]);
-                oldVal.add(clothes[i][0]);
-                cloMap.put(clothes[i][1], oldVal);
-
-            } else {
-                cloMap.put(clothes[i][1], new ArrayList(Arrays.asList(clothes[i][0])));
-            }
-        }
-
-        int answer = 1;
-        Iterator<String> keyIt = cloMap.keySet().iterator();
-        while (keyIt.hasNext()) {
-            int size = cloMap.get(keyIt.next()).size();
-            answer *= (size + 1);
-        }
-        return answer - 1;
+        return Stream.of(clothes).collect(Collectors.groupingBy(s -> s[1], Collectors.counting()))
+                .values()
+                .stream()
+                .reduce(1L, (a, b) -> a * (b + 1))
+                .intValue() - 1;
     }
 }
