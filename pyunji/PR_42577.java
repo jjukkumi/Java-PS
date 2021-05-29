71723fc56e437bc479aad1614da71fcda35a43e8
Pyunji <vusguswl@gmail.com>
2021-05-29 12:51:27 +0900

    Update 해시 안쓰고 풀어봄

--- pyunji/PR_42577.java
+++ pyunji/PR_42577.java
@@ -3,23 +3,9 @@ import java.util.*;
 class Solution {
     public boolean solution(String[] phone_book) {
         Arrays.sort(phone_book);
-
-        Map<Character, ArrayList<String>> preMap = new HashMap<>();
-
-        for (String num : phone_book) {
-            char key = num.charAt(0);
-            ArrayList<String> tmpVal = preMap.getOrDefault(key, new ArrayList<>());
-            tmpVal.add(num);
-            preMap.put(key, tmpVal);
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            if (phone_book[i+1].startsWith(phone_book[i])) return false;
         }
-        for(char key:preMap.keySet()){
-            ArrayList<String> valList = preMap.get(key);
-            for(int i = 0; i< valList.size()-1; i++) {
-                if(valList.get(i + 1).startsWith(valList.get(i)))
-                    return false;
-            }
-        }
-
         return true;
     }
 }
