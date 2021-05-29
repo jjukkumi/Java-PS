cf59f1e6e14ea55d0e31e4f27e9d9e7b30a5eb52
Pyunji <vusguswl@gmail.com>
2021-05-29 12:50:11 +0900

    Update clean code

--- pyunji/PR_42577.java
+++ pyunji/PR_42577.java
@@ -1,19 +1,17 @@
+import java.util.*;
+
 class Solution {
     public boolean solution(String[] phone_book) {
         Arrays.sort(phone_book);
+
         Map<Character, ArrayList<String>> preMap = new HashMap<>();
-        Stream.of(phone_book).forEach(num->{
+
+        for (String num : phone_book) {
             char key = num.charAt(0);
-            if (preMap.containsKey(key)) {
-                ArrayList<String> tmpVal = preMap.get(key);
-                tmpVal.add(num);
-                preMap.put(key, tmpVal);
-            } else {
-                ArrayList<String> tmpVal = new ArrayList<>();
-                tmpVal.add(num);
-                preMap.put(key, tmpVal);
-            }
-        });
+            ArrayList<String> tmpVal = preMap.getOrDefault(key, new ArrayList<>());
+            tmpVal.add(num);
+            preMap.put(key, tmpVal);
+        }
         for(char key:preMap.keySet()){
             ArrayList<String> valList = preMap.get(key);
             for(int i = 0; i< valList.size()-1; i++) {
