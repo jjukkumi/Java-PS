18b14093e1a87e4bef4231fb7da7fb876e415926
Pyunji <vusguswl@gmail.com>
2021-06-09 13:14:03 +0900

    Update follow OOP

--- pyunji/PR_42840.java
+++ pyunji/PR_42840.java
@@ -1,29 +1,62 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.OptionalInt;
+import java.util.*;
+
 
 class Solution {
     public int[] solution(int[] answers) {
-        int[] stu1 = {1, 2, 3, 4, 5};
-        int[] stu2 = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] stu3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        int[][] stus = {stu1, stu2, stu3};
-        int[] stus_score = new int[3];
-        for (int i = 0; i < answers.length; i++) {
-            for (int j = 0; j < stus.length; j++) {
-                if (answers[i] == stus[j][i % stus[j].length]) {
-                    stus_score[j]++;
-                }
-            }
-        }
-        ArrayList<Integer> answer = new ArrayList<>();
-        OptionalInt MAX = Arrays.stream(stus_score).max();
-        for (int i = 0; i < 3; i++) {
-            if (MAX.getAsInt()==stus_score[i]) {
-                answer.add(i+1);
-            }
+        Student[] students = {
+                new Student(1, new int[]{1, 2, 3, 4, 5}),
+                new Student(2, new int[]{2, 1, 2, 3, 2, 4, 2, 5}),
+                new Student(3, new int[]{3, 3, 1, 1, 2, 2, 4, 4, 5, 5}),
 
+        };
+
+        for (int i = 0; i < answers.length; i++) {
+            for (Student student : students) {
+                if (answers[i] == student.getAnswer(i)) student.scoreUp();
+            }
         }
+        
+        int maxScore = 0;
+        for (Student student : students) {
+            if(maxScore < student.getScore()) maxScore = student.getScore();
+        }
+        
+        ArrayList<Integer> answer = new ArrayList<>();
+        for (Student student : students) {
+            if(maxScore==student.getScore()) answer.add(student.getId());
+        }
+        
         return answer.stream().mapToInt(i -> i).toArray();
+    }
+}
+
+class Student {
+    int id;
+    int[] answerList;
+    int score;
+
+    public Student(int id, int[] answerList) {
+        this.id = id;
+        this.answerList = answerList;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getAnswerLength() {
+        return answerList.length;
+    }
+
+    public void scoreUp() {
+        score++;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public int getAnswer(int i) {
+        return answerList[i % getAnswerLength()];
     }
 }
