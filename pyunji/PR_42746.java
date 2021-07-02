aca94b35a28d82d2a8fdaf118499cc615d27b94f
Pyunji <vusguswl@gmail.com>
2021-07-02 13:46:57 +0900

    Update 범용적 방식

--- pyunji/PR_42746.java
+++ pyunji/PR_42746.java
@@ -1,13 +1,16 @@
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.*;
+import java.util.stream.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        if(Arrays.stream(numbers).max().getAsInt()==0) return "0";
-
-        return Arrays.stream(numbers).boxed()
-                .map(integer -> integer + "")
-                .sorted(Comparator.comparing((String s) -> s.repeat(4)).reversed())
-                .reduce((s1, s2) -> s1 + s2).get();
+        Integer[] integerNums = Arrays.stream(numbers).boxed().toArray(Integer[]::new);
+        Arrays.sort(integerNums, (a, b) -> {
+            String as = String.valueOf(a);
+            String bs = String.valueOf(b);
+            return -Integer.compare(Integer.parseInt(as+bs), Integer.parseInt(bs+as));
+        });
+        
+        if (integerNums[0] == 0) return "0";
+        return Arrays.stream(integerNums).map(i->String.valueOf(i)).collect(Collectors.joining());
     }
 }
