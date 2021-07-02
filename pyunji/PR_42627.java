d18a1874578a47cdb68a8ca259e44a8ecd652e9c
Pyunji <vusguswl@gmail.com>
2021-07-02 17:10:03 +0900

    Update

--- pyunji/PR_42627.java
+++ pyunji/PR_42627.java
@@ -8,12 +8,12 @@ class Solution {
         PriorityQueue<int[]> processQ = new PriorityQueue<>(Comparator.comparing((int[] ints) -> ints[0]).thenComparing((int[] ints) -> ints[1]));
         processQ.addAll(Arrays.asList(jobs));
 
+        int[] curProcess = processQ.poll();        
         /* 총 반환시간을 맨 처음 프로세스의 반환 시간으로 초기화한다. */
-        int totalReturnTime = processQ.peek()[1];
-        
+        int totalReturnTime = curProcess[1];
+
         /* 현재의 시점을 맨 처음 프로세스의 요청 시점과 소요시간을 더해 초기화한다. */
-        int[] init = processQ.poll();
-        int curTimePointer = init[0] + init[1];
+        int curTimePointer = curProcess[0] + curProcess[1];
 
         /* 대기하는 프로세스를 소요시간이 짧은 순으로 정렬하는 우선순위 큐를 생성한다. */
         PriorityQueue<int[]> waitingQ = new PriorityQueue<>(Comparator.comparing((int[] ints) -> ints[1]));
@@ -23,7 +23,6 @@ class Solution {
             while (!processQ.isEmpty() && processQ.peek()[0] <= curTimePointer) {
                 waitingQ.add(processQ.poll());
             }
-            int[] curProcess;
             /* 대기하고 있는 프로세스가 없는 경우 */
             if(waitingQ.isEmpty()){
                 /* processQ에서 요청 시점이 가장 빠른 프로세스를 추출한다. */
@@ -41,9 +40,10 @@ class Solution {
                 /* 현재 시점 curTimePointer를 현재 할당된 프로세스의 "소요시간"을 "더해"준다. */
                 curTimePointer += curProcess[1];
                 processQ.addAll(waitingQ);
+                /* waitingQ를 재사용하기 위해 비워준다. */
+                waitingQ.clear();
             }
-            /* waitingQ를 재사용하기 위해 비워준다. */
-            waitingQ.clear();
+            
         }
         return totalReturnTime / jobs.length;
     }
